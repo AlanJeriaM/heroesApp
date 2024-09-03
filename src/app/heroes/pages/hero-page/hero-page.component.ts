a5842315d3635edc20ad340773525af95455342d
Alan Jeria Medina <alanjeria01@gmail.com>
2024-09-03 10:14:05 -0400

    se obtiene el heroe por URL y se crea la pagina del heroe.

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
 
+  public hero?: Hero;
+
+  constructor(private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute, //Proporciona acceso a la información sobre la ruta activa, incluyendo parámetros de la URL.
+              private router: Router, //Router: Se utiliza para navegar entre rutas de la aplicación.
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params //Obtiene los parámetros de la ruta activa, que usualmente contienen el ID del héroe.
+    .pipe(
+      switchMap(({ id }) => this.heroesService.getHeroById( id )), //Toma el ID del héroe desde los parámetros de la ruta y lo pasa al método getHeroById del servicio HeroesService, que devuelve un observable con los datos del héroe.
+    )
+    .subscribe( hero => { //El observable emite un héroe, y este se almacena en la propiedad hero del componente.
+      if( !hero ) return this.router.navigate(['/heroes/list']); //Si no se encuentra un héroe con el ID proporcionado, redirige al usuario a la lista de héroes.
+      this.hero = hero; //Si se encuentra un héroe, se asigna a la propiedad hero para ser mostrado en la plantilla.
+      return;
+    })
+  }
+
+  goBack(){
+    this.router.navigateByUrl('heroes/list')
+  }
 }
 
 
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -15,6 +15,17 @@ export class HeroesService {
 
   getHeroes() : Observable<Hero[]> {  //observable estara emitiendo un arreglo de Hero que lo importamos de las interfaces
       return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`); //obtiene la url del environment.ts + /heroes, El tipo de dato esperado como respuesta es un arreglo de Hero. La función devuelve un Observable que puede ser suscrito en otros componentes para manejar la respuesta.
+  }
+
+
+//Es un método que toma un parámetro id de tipo string.
+//Este método devuelve un Observable que puede emitir un objeto de tipo Hero o undefined.
+  getHeroById(id: string): Observable<Hero | undefined>{
+      return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe( //.pipe(...) se utiliza para encadenar operadores que pueden transformar, filtrar o manejar errores en el flujo de datos del observable.
+        catchError(error => of(undefined) ) // si viene otro endpoint equivocado retorna undefined o of(undefined) crea un observable que emite el valor undefined.
+      )
+
   }
 
 }
